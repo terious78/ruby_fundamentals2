f6d2e8b57a9c70fb078ee16e6db8c952e051e502
terious78 <taesuk.moon@gmail.com>
2016-10-19 15:35:22 -0400

    Final Ex.6

--- exercise6.rb
+++ exercise6.rb
@@ -22,6 +22,26 @@ puts "You need to pick-up #{grocery_list.count.to_i} items."
 #    Otherwise, output "You don't need to pick up bananas today".
 if grocery_list.include?("bananas")
   puts "You need to pick up bananas!"
-else
-  puts "You don't need to pick up bananas today!"
+  else
+    puts "You don't need to pick up bananas today!"
 end
+
+# 4. Display the second item in the list.
+puts "The second item in the list is #{grocery_list[1]}."
+
+# 5. You should sort your grocery list
+#    and output it with asterisks again
+def shopping_list(list)
+  list.each do |grocery|
+    puts "* #{grocery}"
+  end
+end
+
+puts "This is new list."
+shopping_list(grocery_list.sort)
+
+# 6. Delete 'salmon' from the list
+puts "I can't find 'Salmon', I delete."
+grocery_list.delete("salmon")
+
+shopping_list(grocery_list.sort)
