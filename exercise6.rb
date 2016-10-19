4753404bf006fcfcaa7621882bd3021d5e3b56bc
terious78 <taesuk.moon@gmail.com>
2016-10-19 15:11:52 -0400

    Ex. 6-3

--- exercise6.rb
+++ exercise6.rb
@@ -2,7 +2,7 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-# 1. Print 'grocery_list' with asterisk in front
+# 1. Print 'grocery_list' with Asterisk in front of list
 def shopping_list(list)
   list.each do |grocery|
     puts "* #{grocery}"
@@ -10,4 +10,18 @@ def shopping_list(list)
 end
 
 shopping_list(grocery_list)
-  
+
+# 1.Add list 'rice' into 'grocery_list'
+grocery_list << "rice"
+
+# 2. Output the total number of items on your list
+puts "You need to pick-up #{grocery_list.count.to_i} items."
+
+# 3. If your list includes "bananas".
+#    If it does, output "You need to pick up bananas".
+#    Otherwise, output "You don't need to pick up bananas today".
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas!"
+else
+  puts "You don't need to pick up bananas today!"
+end
