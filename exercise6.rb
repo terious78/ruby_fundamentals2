08508ee39690dcf283b7d3b6df1db2b726c7be2a
terious78 <taesuk.moon@gmail.com>
2016-10-19 14:49:22 -0400

    Exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -3,6 +3,11 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 # 1. Print 'grocery_list' with asterisk in front
-grocery_list.each do |list|
-  puts "* #{list}"
+def shopping_list(list)
+  list.each do |grocery|
+    puts "* #{grocery}"
+  end
 end
+
+shopping_list(grocery_list)
+  
